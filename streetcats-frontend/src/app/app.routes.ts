936b62f79eccf04efb0543cd1491429bc67b9810
itssabrinaa <sabrinacassone04@gmail.com>
2025-06-21 17:20:46 +0200

    [FRONTEND] Bootstrap
    Aggiunta di bootstrap e prove varie

--- streetcats-frontend/src/app/app.routes.ts
+++ streetcats-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProvaComponent } from './prova/prova.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: ProvaComponent
+    }
+];
---
+++ streetcats-frontend/src/app/prova/prova.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prova',
+  imports: [],
+  templateUrl: './prova.component.html',
+  styleUrl: './prova.component.scss'
+})
+export class ProvaComponent {
+
+}
