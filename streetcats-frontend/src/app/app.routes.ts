fd605165a66f9dda04345866d711a3daf7554659
itssabrinaa <sabrinacassone04@gmail.com>
2025-06-24 20:49:07 +0200

    [FRONTEND] Navbar e Footer

--- streetcats-frontend/src/app/app.routes.ts
+++ streetcats-frontend/src/app/app.routes.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
-import { ProvaComponent } from './prova/prova.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
     {
-        path:'',
-        component: ProvaComponent
+        path: "",
+        redirectTo: "/home",
+        pathMatch: 'full'
+    }
+    ,
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {
+        path:'login',
+        component: LoginComponent
+    },
+    {
+        path:'signup',
+        component: SignupComponent
     }
 ];
