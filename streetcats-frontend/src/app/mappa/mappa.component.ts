17c1ccc0676362047fe14452ba38dd26b3010571
itssabrinaa <sabrinacassone04@gmail.com>
2025-07-01 10:28:57 +0200

    Ridotta versione Sass
    "overrides": {
        "sass": "1.62.0"
    
    Per dire ad angular di usare una versione più vecchia di sass, evitando warnings di funzioni deprecate in bootstrap

---
+++ streetcats-frontend/src/app/mappa/mappa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mappa',
+  imports: [],
+  templateUrl: './mappa.component.html',
+  styleUrl: './mappa.component.scss'
+})
+export class MappaComponent {
+
+}
