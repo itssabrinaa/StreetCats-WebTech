2b20a11a8c572060d9cffcd717f2163e028f9854
itssabrinaa <sabrinacassone04@gmail.com>
2025-08-29 18:20:29 +0200

    [FRONTEND] Ulteriori controlli token

--- streetcats-frontend/src/app/_interceptors/auth.interceptor.ts
+++ streetcats-frontend/src/app/_interceptors/auth.interceptor.ts
@@ -1,8 +1,13 @@
 import { inject } from '@angular/core';
-import { HttpRequest, HttpHandlerFn } from '@angular/common/http';
+import { HttpRequest, HttpHandlerFn, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 import { AuthService } from '../_services/auth/auth.service';
 
 export function authInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+  const router = inject(Router);
   const authService = inject(AuthService);
   const token = authService.getToken();
 
@@ -13,6 +18,19 @@ export function authInterceptor(request: HttpRequest<unknown>, next: HttpHandler
       }
     });
   }
-  
-  return next(request);
+
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+
+        const serverMsg: string = error.error?.message || error.error?.error || "";
+
+        if (serverMsg.toLowerCase().includes("token")) {
+          authService.logout();
+          router.navigate(['/login'], { queryParams: { unauth: 1 } });
+        }
+      }
+      return throwError(() => error);
+    })
+  );
 }
